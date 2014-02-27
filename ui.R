e005e7c1bb3129d0153cf574bca4e6b66bfa305e
gjaken <gjaken47@gmail.com>
2014-02-27 16:34:23 -0500

    Added tab panel structure, with blank entries waiting to be filled

--- ui.R
+++ ui.R
@@ -8,30 +8,23 @@ shinyUI(pageWithSidebar(
     headerPanel("Michigan Educational Financial Data"),
     
     # Sidebar with a select box input for year
-        sidebarPanel(
-#             sliderInput("range",
-#                         "Select Range for the Map",
-#                         min = 7000,
-#                         max = 50000,
-#                         value = c(7000,10000)
-#                         ), # Need to have this dynamically read from dt, or else scale all to 100
-             
-            uiOutput("outputSlider"), #<-- in ui.R sidebarPanel()
-            
-            selectInput("fldnm",
-                        "Select Financial Measure",
-                        choices = c("Average Teacher Salary"   = "TCHR_SAL.AVG.COUNTY",
-                                    "Expenditure per Pupil"    = "EXP.PER.PUPIL.COUNTY",
-                                    "Revenue per Pupil"        = "REV.PER.PUPIL.COUNTY",                                    
-                                    "Student/Teacher Ratio"    = "PUPIL.PER.TCHR.COUNTY"),
-                        selected = "EXP.PER.PUPIL.COUNTY"
-                        ) # input for dataset choice 
-            
-            
+    sidebarPanel( 
+        uiOutput("outputSlider"), #<-- in ui.R sidebarPanel()
+        
+        selectInput("fldnm",
+                    "Select Financial Measure",
+                    choices = c("Average Teacher Salary"   = "TCHR_SAL.AVG.COUNTY",
+                                "Expenditure per Pupil"    = "EXP.PER.PUPIL.COUNTY",
+                                "Revenue per Pupil"        = "REV.PER.PUPIL.COUNTY",                                    
+                                "Student/Teacher Ratio"    = "PUPIL.PER.TCHR.COUNTY"),
+                    selected = "EXP.PER.PUPIL.COUNTY"
+                    ) # input for dataset choice 
+        
+        
 #         selectInput("year", 
 #                     "Select year:", 
 #                     choices = 2004:2012)
-        ),
+    ),
         
     
     mainPanel(
@@ -42,9 +35,20 @@ shinyUI(pageWithSidebar(
 #         
 #         tableOutput("testTable")
         
-        h3("Michigan Education: Financial Data Choropleth"),
+#         h3("Michigan Education: Financial Data "),
         
-        plotOutput("MIcounty.facet.map", height="600px")
+        tabsetPanel(
+#             tabPanel("State Summary", 
+#                      tableOutput("stateTotals.dt"),
+#                      plotOutput("stateTotals.plot")),
+            tabPanel("County Summary", # also, a Histogram, faceted on year
+                     plotOutput("MIcounty.facet.map", height="600px")),
+            tabPanel("County Summary by Year" # Choropleth and Histogram, by single year
+                     ),            
+            tabPanel("County Drilldown Comparison" #, table of county totals comparison, and plot; 2 selectors
+                     )
+            )
+        
         
     )   
 ))
